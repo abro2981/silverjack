9989013b3d269fa6ccea1d14fa7fe71bbcad3df0
bonedaddy4538 <jsainz@csumb.edu>
2017-02-22 00:54:35 +0000

    notes

--- functions.php
+++ functions.php
@@ -3,7 +3,34 @@
 function getHand(){
     //pick cards
     //add picked card to array and make sure not picked twice
+    //php pop
     global $deck;
+    global $sum1, $sum2, $sum3, $sum4;
+    $deck = array();
+    //create deck
+    for($i = 1; $i < 52;$i ++){
+     $deck[] = $i;
+    }
+    //array for every player
+    $player1 = array();
+    $player2 = array();
+    $player3 = array();
+    $player4 = array();
+    
+    //for player i
+    //loop for palyers and one for points (nested)
+    for($i = 1; $i < 5;$i++){//players getting cards
+    
+    
+        //while sum is score parameters
+    }
+    
+    
+    
+    
+    
+    
+    
     $suitArray = array("clubs","diamonds","hearts","spades");
     $randomIndex  = rand(0,3);
     $randomSuit = $suitArray[$randomIndex];
