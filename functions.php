3093969a36961efdccff0af31a1c58cf6d50ab01
Austin Brown <aubrown@csumb.edu>
2017-02-23 20:13:00 +0000

    got the getHand to work pretty well and the program just spits out the arrays for checking purposes

--- functions.php
+++ functions.php
@@ -11,29 +11,95 @@ function getHand(){
     for($i = 1; $i < 52;$i ++){
      $deck[] = $i;
     }
+    
+    shuffle ($deck);
+    
+    $tempValue;
+    
     //array for every player
     $player1 = array();
-    $player2 = array();
-    $player3 = array();
-    $player4 = array();
     
-    //for player i
-    //loop for palyers and one for points (nested)
-    for($i = 1; $i < 5;$i++){//players getting cards
+    while($sum1 < 38){
+    $tempValue = array_pop($deck);
+    if($tempValue%13 == 0)
+    {
+        $sum1 += 13;
+    }
     
+    else{
+        $sum1 += $tempValue%13;
+    }
     
-        //while sum is score parameters
+    $player1[] = $tempValue;
     }
     
     
+    $player2 = array();
+    
+    while($sum2 < 38){
+    $tempValue = array_pop($deck);
+    
+    if($tempValue%13 == 0)
+    {
+        $sum2 += 13;
+    }
+    
+    else{
+        $sum2 += $tempValue%13;
+    }
+    
+    $player2[] = $tempValue;
+    }
+    
+    $player3 = array();
+     
+    while($sum3 < 38){
+    $tempValue = array_pop($deck);
+    
+   if($tempValue%13 == 0)
+    {
+        $sum3 += 13;
+    }
+    
+    else{
+        $sum3 += $tempValue%13;
+    }
+    
+    $player3[] = $tempValue;
+    }
     
     
+    $player4 = array();
     
+    while($sum4 < 38){
+    $tempValue = array_pop($deck);
     
+   if($tempValue%13 == 0)
+    {
+        $sum4 += 13;
+    }
     
-    $suitArray = array("clubs","diamonds","hearts","spades");
-    $randomIndex  = rand(0,3);
-    $randomSuit = $suitArray[$randomIndex];
+    else{
+        $sum4 += $tempValue%13;
+    }
+    
+    $player4[] = $tempValue;
+    }
+    
+    print_r($player1);
+    echo "<br/>";
+    
+     print_r($player2);
+    echo "<br/>";
+    
+     print_r($player3);
+    echo "<br/>";
+    
+     print_r($player4);
+    echo "<br/>";
+    
+    echo $sum1.$sum2.$sum3.$sum4;
+    
 }
 
 
